c17cbff5962af942f1f64d9b5d4400c8275ed7a9
steven-yn <87164509+steven-yn@users.noreply.github.com>
2022-01-12 12:25:31 +0900

    Rerouting, Pallete

--- pnp-frontend/src/App.js
+++ pnp-frontend/src/App.js
@@ -9,6 +9,8 @@ import PlacePage from './pages/PlacePage';
 import PiecePage from './pages/PiecePage';
 import ProfilePage from './pages/ProfilePage';
 import WritePage from './pages/WritePage';
+import PieceListPage from './pages/PieceListPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
@@ -16,12 +18,14 @@ const App = () => {
       <Route path="/" element={<MainPage />} />
       <Route path="/admin" element={<AdminPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/map/*" element={<MapPage />} />
-      <Route path="/notice/*" element={<NoticePage />} />
-      <Route path="/place/*" element={<PlacePage />} />
-      <Route path="/@:username/:pieceId" element={<PiecePage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/write" element={<WritePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/map" element={<MapPage />} />
+      <Route path="/:noticeId/*" element={<NoticePage />} />
+      <Route path="/:placeId/*" element={<PlacePage />} />
+      <Route path="/:placeId/@:username/*" element={<PieceListPage />} />
+      <Route path="/:placeId/@:username/:pieceId/*" element={<PiecePage />} />
+      <Route path="/:placeId/@:username/write" element={<WritePage />} />
+      <Route path="/@:username/profile" element={<ProfilePage />} />
     </Routes>
   );
 };
---
+++ pnp-frontend/src/pages/PieceListPage.js
@@ -0,0 +1,5 @@
+const PieceListPage = () => {
+  return <div>Piece 목록 페이지</div>;
+};
+
+export default PieceListPage;
